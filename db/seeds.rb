358cadeb1d58e017e455867b49f9c807a8409ffd
William Sinn <w.h.sinn@gmail.com>
2019-04-25 15:18:41 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+students = Student.create([{first_name: 'William', last_name: 'Sinn'},
+                            {first_name: 'Robert', last_name: 'Duh'},
+                            {first_name: 'Dan', last_name: 'TheMan'}])
